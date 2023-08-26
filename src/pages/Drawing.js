27a699a549138de2127da6f023ca89fab88ebdd2
I-HYEON <tjwn1408@naver.com>
2023-08-26 13:01:04 +0900

    :zap: init: 프로젝트 생성

---
+++ src/pages/Drawing.js
@@ -0,0 +1,7 @@
+const Drawing = () => {
+    return ( 
+        <h1>여기는 드로잉페이지</h1>
+        );
+}
+
+export default Drawing;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        <h1>여기는 홈페이지</h1>
+     );
+}
+
+export default Home;
